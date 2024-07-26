9abefb9510dcd3bbf82b34813712b769dddffef1
Allan Bontempo <allanbt@hotmail.com>
2024-07-26 15:52:38 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/example/hruser/resources/UserResource.java
@@ -0,0 +1,39 @@
+package com.example.hruser.resources;
+
+import com.example.hruser.entities.User;
+import com.example.hruser.repositories.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userRepository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findById(@RequestParam String email) {
+        User user = userRepository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+
+
+}
