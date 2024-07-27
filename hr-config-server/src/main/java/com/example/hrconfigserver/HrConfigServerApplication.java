52937fa82b4f8cae12e9ac3980c9908416a469b8
Allan Bontempo <allanbt@hotmail.com>
2024-07-27 17:08:36 -0300

    Security configs to config server

--- hr-config-server/src/main/java/com/example/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/example/hrconfigserver/HrConfigServerApplication.java
@@ -1,5 +1,7 @@
 package com.example.hrconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -7,7 +9,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 @EnableConfigServer
 public class HrConfigServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
 	}
